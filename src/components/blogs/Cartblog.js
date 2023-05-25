29d6c883433a9f273a85247803f3912154536ca2
Kerimov056 <ulvisk@code.edu.az>
2023-05-25 13:57:37 -0700

    blog next

--- src/components/blogs/Cartblog.js
+++ src/components/blogs/Cartblog.js
@@ -1,18 +1,32 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import './blogs.scss'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 
 const Cartblog = ({imgurl,name}) => {
+
+    useEffect(() => {
+        AOS.init({
+            offset: 630,
+            duration: 500,
+            delay: 60,
+        });
+        AOS.refresh();
+    }, []);
+
   return (
     <>
-        <div className='cart'>
+        <div data-aos="zoom-in" className='cart'>
             <div className='cart_img'>
                 <img src={imgurl}></img>
             </div>
             <div className='cart_text'>
                 <h1>{name}</h1>
+                <div>
                 <span className='fashion'>FASHION AND STYLE</span>
                 <span className='month'>January 16, 2019</span>
+                </div>
             </div>
         </div>
     </>
