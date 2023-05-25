d128a86d6b0ef3ef88727b5cff2961ca53217f37
Kerimov056 <ulvisk@code.edu.az>
2023-05-25 15:07:49 -0700

    fouter two start

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/home/Home'
 import Varkala from './components/varkala/Varkala'
 import Abouts from './components/abouts/Abouts';
 import Blogs from './components/blogs/Blogs';
+import Fouter from './components/fouter/Fouter'
 
 function App() {
   return (
@@ -12,11 +13,12 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/Varkala' element={<Varkala/>}/>
-            <Route path='/blog' element={<Blogs/>}/>
-            <Route path='/about' element={<Abouts/>}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/Varkala' element={<Varkala />} />
+          <Route path='/blog' element={<Blogs />} />
+          <Route path='/about' element={<Abouts />} />
         </Routes>
+        <Fouter />
       </Router>
     </>
   );
