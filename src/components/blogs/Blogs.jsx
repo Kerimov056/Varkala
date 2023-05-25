ca415a4fc6c71bc0983d40be6af1407220b26ff9
Kerimov056 <ulvisk@code.edu.az>
2023-05-25 12:53:12 -0700

    blog start

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -8,10 +8,16 @@ const Blogs = () => {
     return (
         <>
             <div className='blog'>
-                
+                <div>
+                    <div>   
+                        <h1>Blog</h1>
+                        <h3>As am hastily invited settled at limited civilly fortune me. Really spring in extent an by. Judge but built party world. Of so am </h3> 
+                        <h3 className='h33'>he remember although required. Bachelor unpacked be advanced at. Confined in declared marianne is vicinity.</h3>
+                    </div>
+                </div>
             </div>
-            <div className='test'>
-
+            <div className=''>
+                as
             </div>
         </>
     )
