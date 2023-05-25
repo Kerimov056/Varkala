3190dfcc43687b99f67589776da6189c822f23b6
Kerimov056 <ulvisk@code.edu.az>
2023-05-25 02:43:16 -0700

    navbar_2 jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './navbar.scss'
+import { Link } from "react-router-dom";
 import { FaFacebookF } from 'react-icons/fa';
 import { AiOutlineTwitter } from 'react-icons/ai';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineHeart } from 'react-icons/ai';
 import { BsTelephone } from 'react-icons/bs';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { BsBasket } from 'react-icons/bs';
 import { FiChevronDown } from 'react-icons/fi';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Navbar = () => {
     return (
@@ -24,17 +30,25 @@ const Navbar = () => {
             </div>
 
 
-            <div className='navbar'>
-                <div className='navbar_in'>
-                    <div className='left'>
-                        <span><FaFacebookF/></span>
-                        <span><AiOutlineTwitter/></span>
-                        <h4><span><BsTelephone/></span>020-800-456-747</h4>
+            <div className='navbar2'>
+                <div className='navbar_in2'>
+                    <div className='left2'>
+                      <ul>
+                        <li>Varkala<FiChevronDown/></li>
+                        <li>Home<FiChevronDown/></li>
+                        <li>Shop<FiChevronDown/></li>
+                        <li>Icons<FiChevronDown/></li>
+                        <li>Pages<FiChevronDown/></li>
+                        <li>Docs<FiChevronDown/></li>
+                      </ul>
                     </div>
                     <h3>Free in-store delivery</h3>
-                    <div className='right'>
-                        <input type="search" />
-                        <button>USD<span><FiChevronDown/></span></button>
+                    <div className='right2'>
+                        <span><input type="search" /><AiOutlineSearch/></span>
+                        <span><BsFillPersonFill/></span>
+                        <span><AiOutlineHeart/></span>
+                        <span><BsBasket/></span>
+                        <span><GiHamburgerMenu/></span>
                     </div>
                 </div>
             </div>
@@ -43,3 +57,7 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+
+{/* <Link to="/Varkala"> */}
+// import { Link } from 'react-router-dom/cjs/react-router-dom.min'
