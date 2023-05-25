02eb19a61f02f93e1f58751de39ffd7ef54c04be
Kerimov056 <ulvisk@code.edu.az>
2023-05-25 02:28:05 -0700

    navbar jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,39 @@
 import React from 'react'
+import './navbar.scss'
 import { FaFacebookF } from 'react-icons/fa';
 import { AiOutlineTwitter } from 'react-icons/ai';
-import { AiOutlineTwitter } from 'react-icons/bs';
-import { FiChevronDown } from 'react-icons/bs';
+import { BsTelephone } from 'react-icons/bs';
+import { FiChevronDown } from 'react-icons/fi';
 
 const Navbar = () => {
     return (
         <>
             <div className='navbar'>
                 <div className='navbar_in'>
-                    <div>
+                    <div className='left'>
                         <span><FaFacebookF/></span>
                         <span><AiOutlineTwitter/></span>
-                        <span><span><BsTelephone/></span>020-800-456-747</span>
+                        <h4><span><BsTelephone/></span>020-800-456-747</h4>
                     </div>
                     <h3>Free in-store delivery</h3>
-                    <div>
+                    <div className='right'>
                         <button>English<span><FiChevronDown/></span></button>
+                        <button>USD<span><FiChevronDown/></span></button>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className='navbar'>
+                <div className='navbar_in'>
+                    <div className='left'>
+                        <span><FaFacebookF/></span>
+                        <span><AiOutlineTwitter/></span>
+                        <h4><span><BsTelephone/></span>020-800-456-747</h4>
+                    </div>
+                    <h3>Free in-store delivery</h3>
+                    <div className='right'>
+                        <input type="search" />
                         <button>USD<span><FiChevronDown/></span></button>
                     </div>
                 </div>
