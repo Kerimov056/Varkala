c826bd00d8c650c3c582fa5d01940b18d914d9ea
Kerimov056 <ulvisk@code.edu.az>
2023-05-25 17:50:26 -0700

    about varkala

--- src/components/abouts/Abouts.jsx
+++ src/components/abouts/Abouts.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
+import './about.scss'
 
 const Abouts = () => {
   return (
     <>
-          about
+      <div className='about'>
+        <div className='about_in'>
+
+          <div className='aboutUS'>
+            <div className='line'>
+              <p></p>
+            </div>
+            <div className='varkala'>
+              <h2>ABOUT US</h2>
+              <h1>We are Varkala</h1>
+              <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections</p>
+            </div>
+          </div>
+
+          <div className='THEOLDTIMES'>
+            <div className='line'>
+              <p></p>
+            </div>
+            <div className='varkala'>
+              <h2>THE OLD TIMES</h2>
+              <h1>History of Varkala</h1>
+            </div>
+          </div>
+
+          <div className='started'>
+            <div className='started_img'>
+              <img src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/photo/raoul-ortega-2JtF6pYAOOI-unsplash.jpg'></img>
+            </div>
+            <div className='started_text'>
+              <h2>HUMBLE BEGINNINGS</h2>
+              <h1>We started as little</h1>
+              <p>The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me?" he thought. It wasn't a dream.</p>
+            </div>
+          </div>
+
+          <div className='grew'>
+            <div className='grew_text'>
+              <h2>VARKALA TODAY</h2>
+              <h1>And then we grew a bit</h1>
+              <p>The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me?" he thought. It wasn't a dream.</p>
+            </div>
+            <div className='grew_img'>
+              <img src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/photo/clark-street-mercantile-P3pI6xzovu0-unsplash.jpg'></img>
+            </div>
+          </div>
+          
+
+        </div>
+      </div>
     </>
   )
 }
