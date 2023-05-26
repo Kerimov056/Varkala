d66756eebadee46f7bdc10b666d46c87a243f60b
Kerimov056 <ulvisk@code.edu.az>
2023-05-25 17:57:31 -0700

    about two share

--- src/components/abouts/Abouts.jsx
+++ src/components/abouts/Abouts.jsx
@@ -49,8 +49,35 @@ const Abouts = () => {
               <img src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/photo/clark-street-mercantile-P3pI6xzovu0-unsplash.jpg'></img>
             </div>
           </div>
-          
 
+        </div>
+      </div>
+
+
+      <div className='Milestones'>
+        <div className='Milestones_in'>
+          <div>
+            <h1>Varkala's Milestones</h1>
+            <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections</p>
+          </div>
+          <div>
+            <div>
+              <h2>1995</h2>
+              <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin</p>
+            </div>
+            <div>
+              <h2>2000</h2>
+              <p>The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared</p>
+            </div>
+            <div>
+              <h2>2010</h2>
+              <p>His room, a proper human room although a little too small, lay peacefully between its four familiar walls. A collection</p>
+            </div>
+            <div>
+              <h2>2018</h2>
+              <p>Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magaz</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
