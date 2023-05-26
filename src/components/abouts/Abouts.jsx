c6ac0c6d432b4c435a67297e734274c961cc796f
Kerimov056 <ulvisk@code.edu.az>
2023-05-26 03:38:02 -0700

    about next

--- src/components/abouts/Abouts.jsx
+++ src/components/abouts/Abouts.jsx
@@ -145,7 +145,18 @@ const Abouts = () => {
           <AliceCarousel mouseTracking items={items} />
         </div>
       </div>
-
+                  
+      <div className='endinfo'>
+          <div className='endinfo_in'>
+                  <div>
+                    <h2>At Varkala, we want the right choice to be as easy as putting on a great T-shirt</h2>
+                  </div>
+                  <div>
+                    <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections</p>
+                    <p>The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me?" he thought. It wasn't a dream.</p>
+                  </div>
+          </div>
+      </div>            
       
     </>
   )
