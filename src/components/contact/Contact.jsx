928934832673e5f6f12ad7ba4519b51b7169c3c2
Kerimov056 <ulvisk@code.edu.az>
2023-05-26 05:01:22 -0700

    contact start

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,8 +6,34 @@ const Contact = () => {
         <>
             <div className='blog'>
                 <div>
-                    <div>   
+                    <div>
                         <h1>Contact</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div className='contact'>
+                <div>
+                    <div>
+                        <div>
+                            <b>Address</b>
+                            <p>13/25 New Avenue<br />New Heaven, 45Y 73J <br />England, Great Britain</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <b>Call center</b>
+                            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <h4>+33 555 444 333</h4>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='end'>
+                            <b>Electronic support</b>
+                            <p>Please feel free to write an email to us or to use our electronic ticketing system.</p>
+                            <h4>info@varkala.com</h4>
+                            <h4>support@varkala.com</h4>
+                        </div>
                     </div>
                 </div>
             </div>
