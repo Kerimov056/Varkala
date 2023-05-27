8352e5087c006a9ad95e253dca437841d481b3f1
Kerimov056 <ulvisk@code.edu.az>
2023-05-27 15:02:27 -0700

    basket design

--- src/components/ToBaskett/ToBasket.jsx
+++ src/components/ToBaskett/ToBasket.jsx
@@ -17,40 +17,40 @@ const ToBasket = ({ id, productName, price, productImage }) => {
                     </div>
 
                     <div className='cart_in'>
-                <div className='productss'>
-                    asdas
-                </div>
-                <div className='Summary'>
-                        <h3><h3>Order Summary</h3></h3>
-                    <div className='Summary_in'>
-                        <p>Shipping and additional costs are calculated based on values you have entered.</p>
-                        <h6></h6>
-                        <div className='Subtotal'>
-                            <span>Order Subtotal</span>
-                            <p>$390.00</p>
+                        <div className='productss'>
+                            asdas
                         </div>
-                        <h6></h6>
-                        <div className='Shipping'>
-                            <h1>Shipping and handling</h1>
-                            <p>$10.00</p>
-                        </div>
-                        <h6></h6>
-                        <div className='Tax'>
-                            <span>Tax</span>
-                            <p>$390.00</p>
-                        </div>
-                        <h6></h6>
-                        <div className='Total'>
-                            <span>Total</span>
-                            <p>$400.00</p>
+                        <div className='Summary'>
+                            <h3><h3>Order Summary</h3></h3>
+                            <div className='Summary_in'>
+                                <p>Shipping and additional costs are calculated based on values you have entered.</p>
+                                <h6></h6>
+                                <div className='Subtotal'>
+                                    <span>Order Subtotal</span>
+                                    <p>$390.00</p>
+                                </div>
+                                <h6></h6>
+                                <div className='Shipping'>
+                                    <h1>Shipping and handling</h1>
+                                    <p>$10.00</p>
+                                </div>
+                                <h6></h6>
+                                <div className='Tax'>
+                                    <span>Tax</span>
+                                    <p>$390.00</p>
+                                </div>
+                                <h6></h6>
+                                <div className='Total'>
+                                    <span>Total</span>
+                                    <p className='cem'>$400.00</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-                </div>
-            </div>
 
-            
+
         </>
     )
 }
