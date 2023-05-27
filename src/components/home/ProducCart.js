33933c05579504f9321e9552f410952ef7f4a759
Kerimov056 <ulvisk@code.edu.az>
2023-05-27 03:11:29 -0700

    Add to cart popup setImg

--- src/components/home/ProducCart.js
+++ src/components/home/ProducCart.js
@@ -1,28 +1,24 @@
-import React, { useState } from 'react'
-import './home.scss'
+import React, { useState } from 'react';
+import './home.scss';
 import { AiOutlineHeart } from 'react-icons/ai';
 import { AiOutlineFullscreen } from 'react-icons/ai';
 import { AiFillStar } from 'react-icons/ai';
 import Modal from 'react-modal';
-// import { Link } from "react-router-dom";
 
+const ProductCart = () => {
+    const [open, setOpen] = useState(false);
+    const [img, setImg] = useState('https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-1-gray.jpg');
 
-const ProducCart = () => {
-
-    const [open, setOpen] = useState(false)
-
-    const [img, setImg] = useState('https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-2-gray.jpg')
-
-    const ChangeImg = (img) => {
-        
-    }
+    const changeImg = (src) => {
+        setImg(src);
+    };
 
     return (
         <>
             <div className='mehsul'>
                 <div className='mehsul_up'>
                     <div>
-                        <img src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/chair.jpg'></img>
+                        <img src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/chair.jpg' alt="Product"></img>
                         <div className='addToCart'>
                             <div>
                                 <h4>Add to cart</h4>
@@ -49,33 +45,35 @@ const ProducCart = () => {
                 </div>
             </div>
 
-
-            <Modal isOpen={open} onRequestClose={() => setOpen(false)} style={{
-                overlay: {
-                    background: "rgb(132, 132, 132)"
-                },
-                content: {
-                    top: '50%',
-                    left: '50%',
-                    right: 'auto',
-                    bottom: 'auto',
-                    marginRight: '-50%',
-                    transform: 'translate(-50%, -50%)',
-                }
-            }}
+            <Modal
+                isOpen={open}
+                onRequestClose={() => setOpen(false)}
+                style={{
+                    overlay: {
+                        background: "rgb(132, 132, 132)"
+                    },
+                    content: {
+                        top: '50%',
+                        left: '50%',
+                        right: 'auto',
+                        bottom: 'auto',
+                        marginRight: '-50%',
+                        transform: 'translate(-50%, -50%)',
+                    }
+                }}
             >
                 <div className='popup'>
                     <div>
                         <div className='pop_left'>
                             <div className='pop_left_img'>
-                                <img src={img}></img>
+                                <img src={img} alt="Selected Image"></img>
                             </div>
                             <div className='pop_left_imgs'>
-                                <img onClick={ChangeImg} src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-1-gray.jpg'></img>
-                                <img onClick={ChangeImg} src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-2-gray.jpg'></img>
-                                <img onClick={ChangeImg} src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-3-gray.jpg'></img>
-                                <img onClick={ChangeImg} src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-4-gray.jpg'></img>
-                                <img onClick={ChangeImg} src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-5-gray.jpg'></img>
+                                <img onClick={() => changeImg('https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-1-gray.jpg')} src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-1-gray.jpg' alt="Thumbnail 1"></img>
+                                <img onClick={() => changeImg('https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-2-gray.jpg')} src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-2-gray.jpg' alt="Thumbnail 2"></img>
+                                <img onClick={() => changeImg('https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-3-gray.jpg')} src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-3-gray.jpg' alt="Thumbnail 3"></img>
+                                <img onClick={() => changeImg('https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-4-gray.jpg')} src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-4-gray.jpg' alt="Thumbnail 4"></img>
+                                <img onClick={() => changeImg('https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-5-gray.jpg')} src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/detail-5-gray.jpg' alt="Thumbnail 5"></img>
                             </div>
                         </div>
 
@@ -86,10 +84,7 @@ const ProducCart = () => {
                 </div>
             </Modal>
         </>
-    )
-}
+    );
+};
 
-export default ProducCart
-
-
-{/* <button onClick={() => setOpen(false)}>Close</button> */ }
+export default ProductCart;
