3d1aec2e5206853cf91c5fe806fd2e5d68e41dea
Kerimov056 <ulvisk@code.edu.az>
2023-05-27 06:42:18 -0700

    cart item

--- src/components/ToBaskett/ToBasket.jsx
+++ src/components/ToBaskett/ToBasket.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './tobasket.scss'
 
-const ToBasket = () => {
+const ToBasket = ({id, productName, price, productImage}) => {
     return (
         <>
-            ToBasket
+            <div className="cart">
+                <div className="cart_in">
+                    <div className="one">
+                        <img src={productImage}></img>
+                    </div>
+                    <div className="two">
+                        <h1>{productName}</h1>
+                        <p>{price}</p>
+                        <button type="button">-</button>
+                        <input value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)}></input>
+                        <button type="button">+</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
