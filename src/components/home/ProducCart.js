39d9eb1da41bb6b144f8fa95b4c148aeac9dca60
Kerimov056 <ulvisk@code.edu.az>
2023-05-27 01:10:55 -0700

    Home cart

--- src/components/home/ProducCart.js
+++ src/components/home/ProducCart.js
@@ -9,29 +9,29 @@ const ProducCart = () => {
         <>
             <div className='mehsul'>
                 <div className='mehsul_up'>
-                    <div className='mehsul_img'>
-                        {/* <img src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/chair.jpg'></img> */}
-                    </div>
-                    <div className='addToCart'>
-                        <div>
-                            <h4>Add to cart</h4>
+                    <div>
+                        <img src='https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/chair.jpg'></img>
+                        <div className='addToCart'>
                             <div>
-                                <span><AiOutlineHeart /></span>
-                                <span><AiOutlineFullscreen /></span>
+                                <h4>Add to cart</h4>
+                                <div>
+                                    <span><AiOutlineHeart /></span>
+                                    <span><AiOutlineFullscreen /></span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className='mehsul_low'>
-                        <h4>Norwegg Chair</h4>
+                    <h4>Norwegg Chair</h4>
                     <div>
                         <span className='qiymet'>$40</span>
                         <span className='star'>
                             <AiFillStar style={{ color: "#bcac76" }} />
                             <AiFillStar style={{ color: "#bcac76" }} />
                             <AiFillStar style={{ color: "#bcac76" }} />
-                            <AiFillStar />
-                            <AiFillStar />
+                            <AiFillStar style={{ color: "rgb(132, 132, 132)" }} />
+                            <AiFillStar style={{ color: "rgb(132, 132, 132)" }} />
                         </span>
                     </div>
                 </div>
