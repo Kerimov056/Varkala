41ff67d3ec38fe7bd7963ce1bba026b5d0b9f1fb
Kerimov056 <ulvisk@code.edu.az>
2023-05-29 10:24:06 -0700

    Baket Responsive Next

--- src/components/ToBaskett/ResponsivBasketCart.js
+++ src/components/ToBaskett/ResponsivBasketCart.js
@@ -1,29 +1,38 @@
 import React, { useContext } from 'react';
 import './tobasket.scss'
-import { ShopContext } from '../content/ShopC'  
+import { ShopContext } from '../content/ShopC'
 
-const ResponsivBasketCart = ({id,imgurl,name,price}) => {
+const ResponsivBasketCart = ({ id, imgurl, name, price }) => {
 
-  const {cartItems,addToCart,removeFromCart,updateCartItemCount,checkout} = useContext(ShopContext)
+    const { cartItems, addToCart, removeFromCart, updateCartItemCount, checkout } = useContext(ShopContext)
 
-  return (
-    <>
-        <div className='dowlandProduct'>
-            <img src={imgurl}></img>
-            <div>
-                <h4>{name}</h4>
-                <p className='sizeL'>Size: Large</p>
-                <p>Colour: Green</p>
+    return (
+        <>
+            <div className='ResponProduc'>
+                <div className='ResponProduc_up'>
+                    <img src={imgurl}></img>
+                    <div>
+                        <h4>{name}</h4>
+                        <p className='sizeL'>Size: Large</p>
+                        <p>Colour: Green</p>
+                    </div>
+                    <span onClick={() => checkout(id)} className='close'>X</span>
+                </div>
+
+                <div>
+                    <div>
+                        <p>Price per item</p><span>${price}</span>
+                    </div>
+                </div>
+
+                <span className='oneSpan'>${price}</span>
+                <p onClick={() => removeFromCart(id)}>-</p>
+                <input value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)}></input>
+                <p onClick={() => addToCart(id)}>+</p>
             </div>
-            <span className='oneSpan'>${price}</span>
-            <p onClick={() => removeFromCart(id)}>-</p>
-            <input value={cartItems[id]}  onChange={(e) => updateCartItemCount(Number(e.target.value),id)}></input>
-            <p onClick={() => addToCart(id)}>+</p>
-            <span onClick={() => checkout(id)} className='close'>X</span>
-       </div>
-       <hr/>
-    </>
-  )
+            <hr />
+        </>
+    )
 }
 
 export default ResponsivBasketCart
