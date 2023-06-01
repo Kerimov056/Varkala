4186caf3597d19f723a1fc56655300087f5593e7
Kerimov056 <ulvisk@code.edu.az>
2023-06-01 04:31:10 -0700

    Heart and like update

--- src/components/heart/Heart.jsx
+++ src/components/heart/Heart.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import ProductCart from '../home/ProducCart'
+import { Homecart } from '../Helpers/Homecart'
 
 const Heart = () => {
   return (
     <>
-        
+        <div>
+            <div>
+                <h1>Products you like</h1>
+                <div>
+                    {
+                        
+                    }
+                </div>
+            </div>
+        </div>
     </>
   )
 }
